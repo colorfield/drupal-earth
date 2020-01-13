0a283998215383d3e1a4bf241da7997cc0c4cb1a
Christophe Jossart <christophe@colorfield.eu>
2020-01-13 22:05:27 +0100

    Remove Pie legends

--- src/components/NivoPie.js
+++ src/components/NivoPie.js
@@ -46,26 +46,6 @@ const NivoPie = (props) =>  {
                 }
             ]}
             fill={props.fills}
-            legends={[
-                {
-                    anchor: 'bottom',
-                    direction: 'row',
-                    translateY: 56,
-                    itemWidth: 100,
-                    itemHeight: 18,
-                    itemTextColor: '#999',
-                    symbolSize: 18,
-                    symbolShape: 'circle',
-                    effects: [
-                        {
-                            on: 'hover',
-                            style: {
-                                itemTextColor: '#000'
-                            }
-                        }
-                    ]
-                }
-            ]}
         />
     )
 }
