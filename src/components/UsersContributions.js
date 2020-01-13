11aa954b5af10e0da3a93b27e80b9d59ef2ff795
Christophe Jossart <christophe@colorfield.eu>
2020-01-13 22:18:09 +0100

    Add contribution types Pie

--- src/components/UsersContributions.js
+++ src/components/UsersContributions.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import Table from './Table';
+import NivoPie from '../components/NivoPie';
 import { getSortedArrayByValue } from '../lib/utils';
 
 const UsersContributions = (props) => {
     const sortedContributions = getSortedArrayByValue(props.usersData.contributions).reverse();
+    const contributions = [];
+    sortedContributions.forEach(contribution => {
+        contributions.push({
+            id: contribution[0],
+            label: contribution[0],
+            value: contribution[1],
+        });
+    });
     return (
-        <Table 
-            title='contribution types'
-            description=''
-            rows={sortedContributions}
-            headers={['Contribution type', 'Core contributors']}
-        />
+        <>
+            <div className="nivo-container">
+                <NivoPie data={contributions} />
+            </div>
+            <Table 
+                title='contribution types'
+                description=''
+                rows={sortedContributions}
+                headers={['Contribution type', 'Core contributors']}
+            />
+        </>
     );
 };
 
